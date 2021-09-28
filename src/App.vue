d900dfe7927039a7a6c61b68bc2d98c1f54ea610
talent jokonya <talentjoko@gmail.com>
2021-09-28 09:58:55 +0200

    first add

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+   <h1>School Management System</h1>
+   <ul>
+   <li v-for="fee of fees" :key="fee.courseId" >
+    {{fee.courseType}}
+   </li>
+   </ul>
+  </div>
+</template>
+
+<script>
+import FeesService from "@/service/FeesService.js";
+
+export default {
+  name: 'App',
+ data(){
+return {
+fees:[
+
+]
+};
+},
+ created(){
+
+  FeesService.getFees().
+then(res=>{
+console.log(res.data)
+})
+.catch(error=>{
+console.log(error)
+});
+
+}
+
+
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul{
+margin: 0px;
+padding:0px;
+}
+
+li{
+list-style:none;
+}
+
+</style>
